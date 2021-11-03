9f97979660cfd51641291f254f3ffe53e8525745
doitduri <kdle_@naver.com>
2021-11-04 03:25:44 +0900

    📝 theme, routes 변경
    - 테마 컬러 변경
    - 안쓰는 routes 삭제

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,5 +5,4 @@ import 'home/view/views.dart';
 
 final Map<String, WidgetBuilder> routes = {
   HomeScreen.routeName: (context) => HomeScreen(),
-  PostingPage.routeName: (context) => PostingPage(),
 };
